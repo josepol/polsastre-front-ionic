a5c84985383b5231767a06beda2c40be9c04b415
Jose Pol Sastre <polsastre3@gmail.com>
2018-05-23 20:59:29 +0200

    upd

--- src/pages/admin/containers/home/admin-home.ts
+++ src/pages/admin/containers/home/admin-home.ts
@@ -1,24 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
 import * as jwtDecode from 'jwt-decode';
 import AuthProvider from '../../../../shared/providers/auth.provider';
 import { BlogDataProvider } from '../../../blog/providers/blog-data.provider';
 import { PostModel } from '../../../blog/model/post.model';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @IonicPage()
 @Component({
   selector: 'app-admin-home',
   templateUrl: 'admin-home.html',
 })
-export class AdminHomePage {
+export class AdminHomePage implements OnInit {
 
+  public addPostFormGroup: FormGroup;
   public currentComponent: string;
   public posts: PostModel[];
 
   constructor(
     private authProvider: AuthProvider,
-    private blogDataProvider: BlogDataProvider
+    private blogDataProvider: BlogDataProvider,
+    private formBuilder: FormBuilder
   ) {
+  }
+
+  ngOnInit(): void {
+    this.addPostFormGroup = this.formBuilder.group({
+      title: [''],
+      subtitle: [''],
+      text: [''],
+      category: ['']
+    });
   }
 
   ionViewDidLoad() {
@@ -34,6 +46,14 @@ export class AdminHomePage {
 
   changeComponent(newComponent) {
     this.currentComponent = newComponent;
+  }
+
+  deletePosts() {
+
+  }
+
+  updatePosts() {
+
   }
 
 }
