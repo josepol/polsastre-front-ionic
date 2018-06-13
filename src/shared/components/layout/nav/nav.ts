868dd8b63ad579541d1cc9030787e618cc48252e
Jose Pol Sastre <polsastre3@gmail.com>
2018-06-13 20:46:56 +0200

    upd

--- src/shared/components/layout/nav/nav.ts
+++ src/shared/components/layout/nav/nav.ts
@@ -6,7 +6,7 @@ import { LoginPage } from '../../../../pages/login/login';
 import { AdminHomePage } from '../../../../pages/admin/containers/home/admin-home';
 import AuthProvider from '../../../providers/auth.provider';
 import { ProfilePage } from '../../../../pages/profile/profile';
-import { ModalController } from 'ionic-angular';
+import { ModalController, ViewController } from 'ionic-angular';
 import { ConfirmationModalComponent } from '../../confirmation-modal/confirmation-modal';
 import MenuComponent from '../menu/menu';
 
@@ -16,6 +16,9 @@ import MenuComponent from '../menu/menu';
 })
 export class NavComponent implements OnInit {
 
+  public isAuthenticated: boolean = false;
+  public isAdmin: boolean = false;
+
   constructor(
     private navigationProvider: NavigationProvider,
     private authProvider: AuthProvider,
@@ -24,18 +27,45 @@ export class NavComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.authProvider.getIsAuthenticated().subscribe(isAuthenticated => this.isAuthenticated = isAuthenticated);
+    this.authProvider.refreshAdmin().then(() => this.isAdmin = true)
+      .catch(() => this.isAdmin = false);
   }
 
   public navigateHome() {
-    this.navigationProvider.getNaviController().push(HomePage, {}, {animate: false});
+    this.navigationProvider.getNaviController().push(HomePage, {}, { animate: false });
+  }
+
+  public navigateBlog() {
+    this.navigationProvider.getNaviController().push(BlogPage, {}, { animate: false });
+  }
+
+  public navigateLogin() {
+    this.navigationProvider.getNaviController().push(LoginPage, {}, { animate: false });
+  }
+
+  public navigateAdminHome() {
+    this.navigationProvider.getNaviController().push(AdminHomePage, {}, { animate: false });
+  }
+
+  public navigateProfile() {
+    this.navigationProvider.getNaviController().push(ProfilePage, {}, { animate: false });
+  }
+
+  public logout() {
+    const confirmationModalComponent = this.modalController.create(ConfirmationModalComponent);
+    confirmationModalComponent.onDidDismiss(data => {
+      if (data.status === 'OK') {
+        this.authProvider.logout();
+      }
+    });
+    confirmationModalComponent.present();
   }
 
   public openMenu() {
     const menuModalComponent = this.modalController.create(MenuComponent);
-    menuModalComponent.onDidDismiss(data => {
-
-    });
+    menuModalComponent.onDidDismiss(data => { });
     menuModalComponent.present();
   }
-  
+
 }
